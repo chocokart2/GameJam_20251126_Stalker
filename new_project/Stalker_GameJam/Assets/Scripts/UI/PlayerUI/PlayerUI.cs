4c10639d225fb00705ed0a61767d00a073d7daf3
jegalsu <chocokart@naver.com>
2025-12-27 03:33:54 +0900

    UI 진행 중-목표 방향

---
+++ new_project/Stalker_GameJam/Assets/Scripts/UI/PlayerUI/PlayerUI.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerUI : MonoBehaviour
+{
+    public List<Transform> MissionPosition;
+    public int missionIndex;
+    public Transform navigatorArrow;
+    public GameObject PushCooltimeUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        navigatorArrow.LookAt(MissionPosition[missionIndex]);
+        PushCooltimeUI.transform.forward = Camera.main.transform.forward;
+    }
+}
